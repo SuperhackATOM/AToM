cc45e931c000dddda6d3cc7a3e23fa3b4bc430b1
thyoondev <th.yoon.dev@gmail.com>
2023-08-13 21:29:07 +0900

    fix : attest call contract

--- src/components/Attest/Attest.tsx
+++ src/components/Attest/Attest.tsx
@@ -2,29 +2,40 @@ import { useAccount, useContractWrite, useNetwork } from "wagmi";
 import styles from "./style.module.scss";
 import { Button, Input, message } from "antd";
 import ATOM_abi from "../../abi/ATOM_abi.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { isAddress } from "viem";
 
 const Attest = () => {
   const { address, isConnected } = useAccount();
   const [messageApi, contextHolder] = message.useMessage();
   const { chain } = useNetwork();
+  const [contractAddress, setContractAddress] = useState<`0x${string}`>("");
   const [addressInput, setAddressInput] = useState("");
   const [locationInput, setLocationInput] = useState("");
   const [contextInput, setContextInput] = useState("");
 
-  const { write: baseWrite } = useContractWrite({
-    address: "0x2BbCDdD17B209dC70493807F62a46a6F3F261072",
-    abi: ATOM_abi,
-    functionName: "mintATOM",
-    args: [[address, addressInput], "0", "0x0102"],
-    onError(error) {
-      console.log("Error", error);
-    },
-  });
+  useEffect(() => {
+    if (chain?.id === 84531) {
+      //base testnet
+      setContractAddress("0x2BbCDdD17B209dC70493807F62a46a6F3F261072");
+    } else if (chain?.id === 999) {
+      //zora testnet
+      setContractAddress("0x2BbCDdD17B209dC70493807F62a46a6F3F261072");
+    }
+  }, []);
 
-  const { write: zoraWrite } = useContractWrite({
-    address: "0x2BbCDdD17B209dC70493807F62a46a6F3F261072",
+  useEffect(() => {
+    if (chain?.id === 84531) {
+      //base testnet
+      setContractAddress("0x2BbCDdD17B209dC70493807F62a46a6F3F261072");
+    } else if (chain?.id === 999) {
+      //zora testnet
+      setContractAddress("0x2BbCDdD17B209dC70493807F62a46a6F3F261072");
+    }
+  }, [chain]);
+
+  const { write } = useContractWrite({
+    address: contractAddress,
     abi: ATOM_abi,
     functionName: "mintATOM",
     args: [[address, addressInput], "0", "0x0102"],
@@ -43,16 +54,8 @@ const Attest = () => {
   const onAttest = () => {
     if (!isAddress(addressInput)) return invalidAddressMessage();
 
-    if (chain?.id === 84531) {
-      //base testnet
-      baseWrite();
-    } else if (chain?.id === 999) {
-      //zora testnet
-      zoraWrite();
-    }
+    write();
   };
-
-  console.log("chain", chain);
 
   return (
     <div className={styles.attest}>
