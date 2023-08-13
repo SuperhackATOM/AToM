aea249e5f168e943ceecc57c67a7d5b2371d6455
thyoondev <th.yoon.dev@gmail.com>
2023-08-13 19:25:45 +0900

    feat : add network

--- src/pages/graph/index.tsx
+++ src/pages/graph/index.tsx
@@ -1,8 +1,18 @@
+import Cytoscape from "@/components/Cytoscape/Cytoscape";
 
 export default function Graph() {
-    return (
-        <div>
-            Social graph page
-        </div>
-    )
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Social graph page
+      <Cytoscape />
+    </div>
+  );
 }
